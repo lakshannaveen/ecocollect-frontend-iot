30e1c00508b1092871dd060a83d460cff0547598
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-01-21 20:23:34 +0530

    animation added to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,8 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home">
-      <h1>Welcome to Home</h1>
+      <h1 className="animated-heading">Welcome to Ecocollect</h1>
+      <h2 className="subheading">IoT-based Smart Waste Collection and Tracking System</h2>
     </div>
   );
 };

ad0969735d15617f37ce54de0824c914118b7975
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-01-21 15:07:20 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Login from './pages/login';
@@ -6,36 +6,29 @@ import Home from './pages/Home';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLoginSuccess = () => {
-    setIsLoggedIn(true); // Set the login state to true after successful login
-  };
-
   return (
     <Router>
       <div className="App">
-        {/* Show Navbar only if logged in */}
-        {isLoggedIn && <Navbar />}
-
         <Routes>
           {/* Route for Login */}
           <Route
             path="/login"
-            element={
-              isLoggedIn ? (
-                <Navigate to="/home" /> // Redirect to Home if already logged in
-              ) : (
-                <Login onSuccess={handleLoginSuccess} />
-              )
-            }
+            element={<Login onSuccess={() => window.location.href = '/home'} />}
           />
 
           {/* Route for Home page */}
           <Route
             path="/home"
-            element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
+            element={
+              <>
+                <Navbar /> {/* Only show Navbar on Home page */}
+                <Home />
+              </>
+            }
           />
+
+          {/* Default route (redirect to login if no matching route) */}
+          <Route path="/" element={<Navigate to="/login" />} />
         </Routes>
       </div>
     </Router>

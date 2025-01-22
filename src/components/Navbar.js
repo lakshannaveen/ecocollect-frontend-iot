d3bc9dd022251f5e057bf8a7a4d7e5fc735b5952
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-01-22 11:47:08 +0530

    navbar updated active link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,23 +6,35 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <NavLink to="/home" className={({ isActive }) => (isActive ? 'active-link' : '')}>
+        <NavLink 
+          to="/home" 
+          className={({ isActive }) => (isActive ? 'active-link' : '')}
+        >
           EcoCollect
-        </NavLink> {/* Add Link to home page */}
+        </NavLink>
       </div>
       <ul className="navbar-links">
         <li className="navbar-item">
-          <NavLink to="/bin" className={({ isActive }) => (isActive ? 'active-link' : '')}>
+          <NavLink 
+            to="/bin" 
+            className={({ isActive }) => (isActive ? 'active-link' : '')}
+          >
             Bin Status
           </NavLink>
         </li>
         <li className="navbar-item">
-          <NavLink to="/maps" className={({ isActive }) => (isActive ? 'active-link' : '')}>
+          <NavLink 
+            to="/maps" 
+            className={({ isActive }) => (isActive ? 'active-link' : '')}
+          >
             Bin Maps
-          </NavLink> {/* Navigate to Map */}
+          </NavLink>
         </li>
         <li className="navbar-item">
-          <NavLink to="/analysis" className={({ isActive }) => (isActive ? 'active-link' : '')}>
+          <NavLink 
+            to="/analysis" 
+            className={({ isActive }) => (isActive ? 'active-link' : '')}
+          >
             Analysis
           </NavLink>
         </li>
